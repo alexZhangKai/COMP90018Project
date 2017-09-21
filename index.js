094e0cef92811fd2c464f6a75365b2fa5a50ff2b
Alex <alexzhang1931@gmail.com>
2017-09-21 11:27:56 +1000

    initial

---
+++ index.js
@@ -0,0 +1,208 @@
+var request = require("request");
+var express = require("express");
+var bodyParser = require("body-parser");
+var azure = require("azure-storage");
+var blobService = azure.createBlobService("faceimg", "vjckPyJ37aWuElE81It17cMOZvy54+1pAXYEQWzmRyCqlqpYEOpST6ZZ1LO1dgtwtjs5P7wV3Bwih3B5q9vUrg==");
+var multer = require("multer");
+var storage = multer.memoryStorage();
+var upload = multer({storage: storage});
+
+const subKey = "3557f36bcd7d45edb927993db27a47fb";
+const fileUrl = "https://faceimg.blob.core.windows.net/faceimgs/testBlob";
+var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// app.all("*", function(req, res, next){
+//   console.log("in app.all");
+//   res.writeHead(200, {"content-type":"text/plain"});
+//   next();
+// });
+
+app.get("/createFaceGroup", function(req, res){
+  createFaceGroup(req.query.groupName, res);
+});
+
+app.get("/createPerson", function(req, res){
+  createPerson(req.query.personName, res);
+});
+
+app.get("/listAllPerson", function(req, res){
+  listAllPerson(res);
+});
+
+app.get("/deletePerson", function(req, res){
+  deletePerson(req.query.personID, res);
+});
+
+app.post("/addFace", upload.single("photo"), function(req, res){
+  var buffer = new Buffer(req.file.buffer);
+  uploadFile(buffer, res);
+  addFace(req.query.personID, res);
+  trainPersonGroup();
+});
+
+app.post("/identify", upload.single("photo"), function(req, res){
+  var buffer = new Buffer(req.file.buffer);
+  uploadFile(buffer, res);
+  var faceid = detectFace(res);
+  //identify(faceid, res);
+});
+
+app.listen(3780);
+
+let Duplex = require('stream').Duplex;
+function bufferToStream(buffer) {
+  let stream = new Duplex();
+  stream.push(buffer);
+  stream.push(null);
+  return stream;
+}
+
+function createFaceGroup(groupName, client){
+  var options = {
+    method: "PUT",
+    url: "https://southeastasia.api.cognitive.microsoft.com/face/v1.0/persongroups/facedb",
+    headers:
+     {"content-type": "application/json",
+      "ocp-apim-subscription-key": subKey},
+    body: {name: groupName},
+    json: true
+  };
+
+  request(options, function(error, response, body){
+    if (error) console.log(error);
+    client.send(body);
+  });
+}
+
+function createPerson(personName, client){
+  var options = {
+    method: "POST",
+    url: "https://southeastasia.api.cognitive.microsoft.com/face/v1.0/persongroups/facedb/persons",
+    headers:
+     {"content-type": "application/json",
+      "ocp-apim-subscription-key": subKey},
+    body: {name: personName},
+    json: true
+  };
+
+  request(options, function(error, response, body){
+    if(error) console.log(error);
+    client.send(body);
+  });
+}
+
+function listAllPerson(client){
+  var options = {
+    method: "GET",
+    url: "https://southeastasia.api.cognitive.microsoft.com/face/v1.0/persongroups/facedb/persons",
+    headers:
+     {"content-type": "application/json",
+      "ocp-apim-subscription-key": subKey},
+    json: true
+  };
+
+  request(options, function(error, response, body){
+    if(error) console.log(error);
+    client.send(body);
+  });
+}
+
+function deletePerson(personID, client){
+  var options = {
+    method: "DELETE",
+    url: "https://southeastasia.api.cognitive.microsoft.com/face/v1.0/persongroups/facedb/persons/"+personID,
+    headers:
+     {"content-type": "application/json",
+      "ocp-apim-subscription-key": subKey},
+    json: true
+  };
+
+  request(options, function(error, response, body){
+    if(error) console.log(error);
+    client.send(body);
+  });
+}
+
+function uploadFile(buffer, client){
+  var stream = bufferToStream(buffer);
+  blobService.createBlockBlobFromStream("faceimgs", "testBlob", stream, buffer.length, function(error){
+    if(error){
+      client.send(error);
+    };
+  });
+}
+
+function addFace(personID, client){
+  var options = {
+    method: "POST",
+    url: "https://southeastasia.api.cognitive.microsoft.com/face/v1.0/persongroups/facedb/persons/"+personID+"/persistedFaces",
+    headers:
+     {"content-type": "application/json",
+      "ocp-apim-subscription-key": subKey},
+    body: {url: fileUrl},
+    json: true
+  };
+
+  request(options, function(error, response, body){
+    if(error) console.log(error);
+    client.send(body);
+  });
+}
+
+function trainPersonGroup(){
+  var options = {
+    method: "POST",
+    url: "https://southeastasia.api.cognitive.microsoft.com/face/v1.0/persongroups/facedb/train",
+    headers:
+     {"content-type": "application/json",
+      "ocp-apim-subscription-key": subKey},
+    json: true
+  };
+
+  request(options, function(error, response, body){
+    if(error) console.log(error);
+  });
+}
+
+function detectFace(client){
+  var options = {
+    method: "POST",
+    url: "https://southeastasia.api.cognitive.microsoft.com/face/v1.0/detect",
+    headers:
+     {"content-type": "application/json",
+      "ocp-apim-subscription-key": subKey},
+    body: {url: fileUrl},
+    json: true
+  };
+
+  request(options, function(error, response, body){
+    if(error) console.log(error);
+    identify(body[0].faceId, client);
+  });
+}
+
+function identify(faceid, client){
+  var lst = [];
+  lst.push(faceid);
+  var options = {
+    method: "POST",
+    url: "https://southeastasia.api.cognitive.microsoft.com/face/v1.0/identify",
+    headers:
+     {"content-type": "application/json",
+      "ocp-apim-subscription-key": subKey},
+    body: {
+      personGroupId: "facedb",
+      faceIds: lst,
+      maxNumOfCandidatesReturned: 1,
+      confidenceThreshold: 0.5
+    },
+    json: true
+  };
+
+  request(options, function(error, response, body){
+    if(error) console.log(error);
+    client.send(body);
+  });
+}
