c0ce5cac7c7e08a797170e2776256afa7e18640d
Alex <alexzhang1931@gmail.com>
2017-09-27 16:07:05 +1000

    Create face group and person from index.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,48 @@
 $(function(){
-  console.log('test');
-  $('#testButton').on('click', function(){
-    alert('success');
+  $('#createFaceGroup').on('click', function(){
+      var newGroupName = $('#groupName').val() || 'facedb';
+      var params = {
+        groupName: newGroupName
+      };
+      // var url = window.location.hostname + '/api/createFaceGroup?' + $.param(params);
+      var url = '/api/createFaceGroup?' + $.param(params);
+      console.log(url);
+      $.ajax({
+        type: 'GET',
+        url: url,
+        dataType: 'jsonp',
+        success: function(data){
+          console.log(data);
+          $('#CgroupResult').val(data.responseText);
+        },
+        error: function(err){
+          console.log(err.responseText);
+          $('#CgroupResult').val(err.responseText);
+        }
+      });
   });
+
+  $('#createPerson').on('click', function(){
+      var newPersonName = $('#personName').val() || 'person';
+      var params = {
+        personName: newPersonName
+      };
+      // var url = window.location.hostname + '/api/createFaceGroup?' + $.param(params);
+      var url = '/api/createPerson?' + $.param(params);
+      console.log(url);
+      $.ajax({
+        type: 'GET',
+        url: url,
+        dataType: 'jsonp',
+        success: function(data){
+          console.log(data);
+          $('#CpersonResult').val(data.responseText);
+        },
+        error: function(err){
+          console.log(err.responseText);
+          $('#CpersonResult').val(err.responseText);
+        }
+      });
+  });
+
 });
